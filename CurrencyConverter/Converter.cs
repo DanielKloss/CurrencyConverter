55731c87a788470e0e6cf5d59f4358d5a2ffc765
hmohamed7 <hannah.mohamed@fdmgroup.com>
2015-11-18 09:17:24 +0000

    Converter with some initial code

--- CurrencyConverter/Converter.cs
+++ CurrencyConverter/Converter.cs
@@ -8,14 +8,43 @@ namespace CurrencyConverter
 {
     public class Converter
     {
+        bool toEuros;
+        string currency;
+        double amount;
+        double rate;
+        double convertedMoney;
+
+        public Converter(bool ToEuros, string Currency, double Amount)
+        {
+            toEuros = ToEuros;
+            currency = Currency;
+            amount = Amount;
+        }
         Double GetRate()
         {
-            return 0.0;
+            //Here we would put DailyData.GetDictionary & then a foreach loop where it compares the
+            //currency code with the key of the Dictionary returning the value associated
+            //with that key
+            //Dictionary rates(currencyCode, rateVal) = DailyData.GetDictionary();
+            //foreach (currencyCode in rates){
+            //  if (currencyCode == currency){
+            //      return rateVal;
+            //  }
+            //rate = rateVal;
+            return rate;
         }
 
         Double Convert()
         {
-            return 0.0;
+            if (toEuros == true)
+            {
+                convertedMoney = 1.00 / (amount * rate);
+            }
+            else
+            {
+                convertedMoney = amount * rate;
+            }
+            return convertedMoney;
         }
     }
 }
